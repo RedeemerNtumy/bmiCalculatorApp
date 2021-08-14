6ab7423bad1adce5fdda48917963b472a3fa8f1f
Redeemer Ntumy <ntumyredeemer@gmail.com>
2021-08-14 15:34:48 +0000

    Added some packages and icons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const reusableCardNativeColor = Colors.blue;
@@ -25,6 +26,22 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: BmiCard(
+                    cardChild: Column(
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "MALE",
+                          style: TextStyle(color: Colors.white,
+                          fontSize: 20),
+                        )
+                      ],
+                    ),
                     nativeColor: reusableCardNativeColor,
                   ),
                 ),
@@ -70,12 +87,14 @@ class _InputPageState extends State<InputPage> {
 }
 
 class BmiCard extends StatelessWidget {
-  BmiCard({required this.nativeColor});
+  BmiCard({required this.nativeColor, this.cardChild});
   final Color nativeColor;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
